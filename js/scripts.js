90dbe9e5a8c0e3709d34b2f6db72041631bed8bd
Calla Rudolph and Max Scher <max@>
2017-06-06 08:37:36 -0700

    add first pass at js

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $(".count").submit(function(event) {
+    event.preventDefault();
+
+    var countTo = parseInt($("input#count-to").val());
+    var countBy = parseInt($("input#count-by").val());
+
+    var numbersCounted = [];
+
+    for (var i = 0; i <= countTo; i += countBy) {
+      numbersCounted.push(i);
+    }
+
+    $("#output").after(numbersCounted);
+    $(".result").show();
+
+  });
+});
