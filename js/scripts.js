36a5dc2ca561806d56f528cc9b3b4f4806f27698
Calla Rudolph and Max Scher <calla@>
2017-06-06 10:37:30 -0700

    add if-else statements to create alerts for non-numeric inputs

--- js/scripts.js
+++ js/scripts.js
@@ -4,14 +4,25 @@ $(document).ready(function() {
 
     var countTo = parseInt($("input#count-to").val());
     var countBy = parseInt($("input#count-by").val());
-
+    console.log(countTo)
     var numbersCounted = [];
 
-    for (var i = 0; i <= countTo; i += countBy) {
-      numbersCounted.push(i + ", ");
+    if (isNaN(countTo) || isNaN(countBy)) {
+      alert("Numbers only, please!");
+    } else if (countTo < 0 || countBy < 0) {
+      alert("Please use positive numbers.")
+    } else if (countBy > countTo) {
+      alert("You can't count to " + countTo + " when you're counting by " + countBy + ", because " + countBy + " is larger than " + countTo + "!")
     }
 
 
+    for (var i = countBy; i <= countTo; i += countBy) {
+      numbersCounted.push(i + ", ");
+      if (i === countTo) {
+        delete(numbersCounted[numbersCounted.length - 1]);
+        numbersCounted.push(i);
+      }
+    };
 
     $("#output").empty();
     $("#output").append(numbersCounted);
